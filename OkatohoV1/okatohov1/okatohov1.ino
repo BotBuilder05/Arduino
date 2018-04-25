cb243f45ae9830cd10575642bd49444331f18ef6
manosoleil <manosoleil@yahoo.fr>
2018-04-25 19:14:59 +0200

    mise a jour strategie

--- OkatohoV1/okatohov1/okatohov1.ino
+++ OkatohoV1/okatohov1/okatohov1.ino
@@ -1,84 +1,126 @@
 /*
 # Strategie
 * Mise sous tension -> initialisation servo moteur (pied en particulier pour avoir la position de depart)
-* Appuie sur boutojn cote adversaire (bouton droit ou bouton gauche)
+* Appuie sur bouton cote adversaire (bouton droit ou bouton gauche)
 * Attendre 5 secondes
 * Demarrage jupe
 * Avance
   - si dojo Tourne de 10°
 * Detection
   - si detection avance
-  - si non Tourne 1°
+  - si non Tourne 10°
 */
 #define DEBUG
 //#define SLOW 
 
 //Variable Machine d'etat
-#define START 1
-#define WAIT 2
-#define SKIRT 3
-#define FORWARD 4
-#define SEARCH 5
+#define DEPART 1
+#define ATT_5_SEC 2
+#define JUPE 3
+#define ATTAQUE_AVEUGLE 4
+#define CHERCHE_ADV 5
+#define ATTAQUE 6
+
+int s_state , s_state_next;
+
 
 void setup() {
 	#ifdef DEBUG
 		Serial.begin(9600 );
 	#endif
 	// On initialise la machine d'etat 
-	s_state=START;
+	s_state=DEPART;
 }
 
-int detectDojo() {
+int detection() {
 	/* This finction should
-		* return 0 if it detects dojo
+		* return 0 if it detects 
 		* return 1 othewise
-	This should be called by Timer interruption
+	"This should be called by Timer interruption" pas sure!
+
+ 	- lit 1 fois chaque capteurs et on boucle 2 fois -> TABLEAUX
+	- Detection si les 2 valeurs pour un meme capteurs sont superieures au seuil
+	- Renvoir si detection ou non (boolean)
+	- Affecte la variable de direction si detection
 	*/
 	return 0
 }
 
+void tourne(int Angle_Souhaite) {
+	/*
+  	- on met le servo de marche (servoM) dans la position basse
+  	- on met le servo de direction (servoD) line 84 de etat marche +10 
+  	- on met le servo de marche (servoM) dans la posistion en debut de fonction tourne 10
+  	- on remet le pied a l'origine on met le servo de direction (servoD) line 84 de etat marche:w
+  	*/
+	return 0
+}
+
+identifyButtonPress();
+waitButtonPres();
+wait();
+skirtStart();
+void Avance(int Direction_Souhaite){};
+
 void loop() {
 	switch (s_state) {
-		case START:
+		case DEPART:
 			#ifdef DEBUG
         			Serial.println("DEPART");
 			#endif
 			identifyButtonPress();
 			waitButtonPres();
 		break;
-		case WAIT:
+		case ATT_5_SEC:
 			#ifdef SLOW
 				delay(attente);
 			#endif
 			#ifdef DEBUG
-				Serial.println("WAIT");
+				Serial.println("ATT_5_SEC");
 			#endif
 			wait();
-			s_state_next=SKIRT;
+			s_state_next=JUPE;
 		break;
-		case SKIRT:
+		case JUPE:
 			#ifdef DEBUG
-				Serial.println("SKIRT START");
+				Serial.println("JUPE DEPART");
 			#endif
 			skirtStart();
-			s_state_next=FORWARD;
+			s_state_next=ATTAQUE_AVEUGLE;
 		break;
-		case FORWARD:
+		case ATTAQUE_AVEUGLE:
 			#ifdef DEBUG
-				Serial.println("Move FORWARD");
+				Serial.println("ATTAQUE_AVEUGLE");
 			#endif
-			/* If Dojo tourne 10)*/
-			if (detect_dojo == 0) {
-				s_state_next=TURN;
+			/* 	- on commence par devant ou gauche en fonction du bouton apuyer pour le demarrage
+  				- avance(devant) ou gauche
+ 				- apeler la fonction detect -> si Vrai : passe a l'ETAT Attaque (avec direction de detec)
+  				- avance(devant) ou arriere
+  				- apeler la fonction detect -> si vrai : idem
+  				et on boucle pendant 3 pas de chaque cote sauf si detection (si on a implementer les interruptions)
+  				- puis on passe a l'ETAT CHERCHE_ADV
+  			*/
+		break;
+		case ATTAQUE:
+			#ifdef DEBUG
+				Serial.println(" ATTAQUE");
+			#endif
+			/*	- Avance x pas
+				- detect
+			*/
+			Avance(int Direction_Souhaite);
+
+			if (detection == 0) {
+				s_state_next=CHERCHE_ADV;
 			}
 			else {
-				/*else we need to search the other*/
-				s_state_next=SEARCH;
+				/*Peut etres en trop??*/
+				s_state_next=ATTAQUE;
 			}
 		break;
-		case TURN:
+		case CHERCHE_ADV:
 			#ifdef DEBUG
-				Serial.println("TURN");
+				Serial.println("CHERCHE_ADV");
 			#endif
 			/* Move the foot in order to turn the robot*/
 			/* if detect_dojo=0 then turn=10° otherwise turn=10°)*/
@@ -89,18 +131,18 @@ void loop() {
 				turn(1);
 			}
 		break;
-		case SEARCH:
+		case CHERCHE_ADV:
 			#ifdef DEBUG
-				Serial.println("SEARCH");
+				Serial.println("CHERCHE_ADV");
 			#endif
 			detect();
-			/* if find someone forward*/
+			/* if find someone ATTAQUE*/
 			if (detect == 0) {
-				s_state_next=FORWARD;
+				s_state_next=ATTAQUE;
 			}
 			else {
 				/*else tourne 1°*/
-				s_state_next=TURN;
+				s_state_next=CHERCHE_ADV;
 			}
 			
 		break;
