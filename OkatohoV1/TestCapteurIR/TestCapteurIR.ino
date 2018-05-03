03d50dcabf4d78ad47845928bd80fb490d5258ed
Xavier Charles <xavier.charles@vocado.com>
2018-05-03 09:31:25 +0200

    fix print

--- OkatohoV1/TestCapteurIR/TestCapteurIR.ino
+++ OkatohoV1/TestCapteurIR/TestCapteurIR.ino
@@ -27,7 +27,8 @@ void lectureCapteur(int capteur) {
 	int valeurLue=0;
 
 	valeurLue = analogRead(capteur);
-	Serial.print(capteur, " : ");
+	Serial.print(capteur);
+	Serial.print(" : ");
 	Serial.println(valeurLue);
 	delay(500);
 }
